90bb3dc658d0be1feeb72e785ab79aef8eb051e0
shrujm4 <shrujm4@gmail.com>
2019-07-30 22:34:57 +0100

    'java'

---
+++ src/PersonalDetails.java
@@ -0,0 +1,25 @@
+public class PersonalDetails {
+
+    public static void main (String argv []) {
+
+        String name = "Simon Lee";
+        String address = "105, St Paul's Avenue, Harrow, HA3 9PT";
+        String age = "25";
+        String mobile = "07934587909";
+        String salary = "30,000";
+
+
+
+
+
+        System.out.println("Name = " + name);
+        System.out.println("Address = "+ address);
+        System.out.println("Age = " + age + "yrs");
+        System.out.println("Mobile = " + mobile);
+        System.out.println("Salary = " + salary + "per Annum");
+
+
+
+    }
+
+}
